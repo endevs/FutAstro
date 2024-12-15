fd66a12f0af2747397a0a351c287d73364edee57
Lenovo <Lenovo@DESKTOP-7HTC3H9>
2024-12-15 22:21:47 +0530

    Fast Commit

---
+++ FutAstro/FutAstro/src/main/java/com/javatechie/spring/drools/api/MegaOfferController.java
@@ -0,0 +1,22 @@
+package com.javatechie.spring.drools.api;
+
+import org.kie.api.runtime.KieSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MegaOfferController {
+	@Autowired
+	private KieSession session;
+
+	@PostMapping("/order")
+	public Order orderNow(@RequestBody Order order) {
+		session.insert(order);
+		session.fireAllRules();
+		System.out.println("order "+order.getName()+" Discount"+order.getDiscount()+" Bank "+order.getCardType());
+		return order;
+	}
+
+}
